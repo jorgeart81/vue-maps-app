1d352be3de81b2e981c0a01597d5a61fb18b29b1
Jorge Reyes <92123185+JorgeArt81@users.noreply.github.com>
2024-05-21 20:19:39 -0500

    Create .env

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,19 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
-import store from './store'
-import router from './router'
+// import dotenv from 'dotenv';
+// dotenv.config();
+
+import { createApp } from 'vue';
+import './style.css';
+import App from './App.vue';
+import store from './store';
+import router from './router';
 
 import mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
 
-mapboxgl.accessToken = 'pk.eyJ1IjoiYWVyb2VzdGFkaXN0Y2EiLCJhIjoiY2wxY3RxcjZlMDBpYjNqcXN4bWswNTU4dyJ9.LTRkLdqgDrj6Hjftspvf8w'
+mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_ACCESS_TOKEN;
 
 if (!navigator.geolocation) {
-    alert('Tu navegador no soporta el GeoLocation')
-    throw new Error('Tu navegador no soporta el GeoLocation')
+  alert('Tu navegador no soporta el GeoLocation');
+  throw new Error('Tu navegador no soporta el GeoLocation');
 }
 
-createApp(App)
-    .use(store)
-    .use(router)
-    .mount('#app')
+createApp(App).use(store).use(router).mount('#app');
